ca614742814bfd137c7a19238b49d026fc297e1c
Velizar Shulev <velizar.shulev@gmail.com>
2024-06-01 15:49:19 +0300

    Add example script

---
+++ example_inference.py
@@ -0,0 +1,15 @@
+import torch
+from transformers import PreTrainedTokenizerFast, BertForMaskedLM, BertConfig
+
+model = BertForMaskedLM.from_pretrained("LofiAmazon/BarcodeBERT-Entire-BOLD")
+model.eval()
+
+tokenizer = PreTrainedTokenizerFast.from_pretrained("LofiAmazon/BarcodeBERT-Entire-BOLD")
+
+inputs = tokenizer("ACTG GTCA GGAA", return_tensors="pt")
+
+with torch.no_grad():
+    outputs = model(**inputs).hidden_states[-1]
+    outputs = outputs.mean(1).squeeze()
+
+print(outputs)
